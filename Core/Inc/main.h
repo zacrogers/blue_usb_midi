3bb06d188fa5199c03697b40d7f1073424c413c7
rogerszach <z.m.rogers@gmail.com>
2020-11-02 10:42:52 +1300

    Keypad menu navigation working

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -37,44 +37,59 @@ extern "C" {
 
 /* Exported types ------------------------------------------------------------*/
 /* USER CODE BEGIN ET */
-typedef enum
-{
-	STATE_KEYPAD,
-	STATE_SEQUENCER
-}State;
 
+/* The mode represents the method used to send
+ * midi data to the host computer
+ */
 typedef enum
 {
 	MODE_KEYPAD,
 	MODE_SEQUENCER
 }Mode;
 
-typedef struct
+/*
+ *  Each mode has a main and options screen.
+ *  The main screen displays the current relevant values for each mode.
+ *  The options screen is used for navigation and setting of the various values.
+ */
+typedef enum
 {
-	uint8_t note;
-	uint8_t velocity;
-	uint8_t octave;
+	SC_MAIN,
+	SC_OPTIONS
+}Screen;
+
+/* This is used to control what value is currently being changed by the encode r*/
+typedef enum
+{
+	ENC_KB_OPTIONS,
+	ENC_KB_VAR_OCTAVE,
+	ENC_KB_VAR_VELOCITY,
+	ENC_SQ_OPTIONS,
+	ENC_SQ_VAR_TEMPO,
+	ENC_SQ_VAR_LENGTH
+}EncoderVar;
+
+/* The variable for the keyboard and sequqncer modes are both stored in arrays.
+ * The two following enums are used for indexing these arrays
+ */
+typedef enum
+{
+	KB_VAR_OCTAVE,
+	KB_VAR_VELOCITY,
+	KB_VAR_NOTE,
+	KB_VAR_N
 }KbVars;
 
-typedef struct
+typedef enum
 {
-	uint8_t bpm;
-	bool playing;
-	uint8_t note;
-	uint8_t step;
+	SQ_VAR_BPM,
+	SQ_VAR_LENGTH,
+	SQ_VAR_NOTE,
+	SQ_VAR_PLAYING,
+	SQ_VAR_STEP,
+	SQ_VAR_N
 }SequencerVars;
 
-typedef enum
-{
-	MAIN_STATE_KEYPAD,
-	MAIN_STATE_SEQUENCER
-}MainState;
-
-typedef enum
-{
-	KP_STATE_ROOT,
-	KP_STATE_OCTAVE
-}KeypadState;
 /* USER CODE END ET */
 
 /* Exported constants --------------------------------------------------------*/
@@ -103,36 +118,41 @@ void key_down_handler(const char key);
 
 void encoder_timer_init(void);
 void encoder_button_it_init(void);
+
+/* Handles the mapping of what value the encoder is controlling */
 void handle_encoder(void);
 
+/* Updates a variable from the timer count value
+ * and wraps values around at the limits */
+void update_encoder(uint8_t min, uint8_t max, uint8_t *curr_val, uint8_t *prev_val);
+void handle_encoder_btn(void);
+
+/* Redraw the menu */
 void update_menu(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
 /* USER CODE BEGIN Private defines */
-#define OB_LED_PORT GPIOC
-#define OB_LED_PIN GPIO_PIN_13
+#define OB_LED_PORT      GPIOC
+#define OB_LED_PIN       GPIO_PIN_13
 
-#define ENC_BTN_PORT GPIOC
-#define ENC_BTN_PIN GPIO_PIN_14
+#define ENC_BTN_PORT     GPIOC
+#define ENC_BTN_PIN      GPIO_PIN_14
 
 #define MAX_MIDI_OCTAVES 10
+#define MAX_MIDI_NOTE    127
+#define MAX_VELOCITY     127
 
-#define MIN_MIDI_NOTE 0
-#define MAX_MIDI_NOTE 127
+#define NUM_SEMITONES    12
+#define NUM_BUTTONS      16
 
-#define MIN_VELOCITY 0
-#define MAX_VELOCITY 127
+#define MAX_BPM          50
+#define MIN_BPM          200
 
-#define NUM_SEMITONES 12
-#define NUM_SEMITONES 12
-#define NUM_BUTTONS   16
-
-#define N_MENU_OPTS 4
 /* Defines for menu options */
-#define N_MODES   2
-#define N_KB_OPTS 2
-#define N_SEQ_OPTS 3
+#define N_MODES     2
+#define N_KB_OPTS   2
+#define N_SEQ_OPTS  3
 #define N_SEQ_STEPS 8
 /* USER CODE END Private defines */
 
